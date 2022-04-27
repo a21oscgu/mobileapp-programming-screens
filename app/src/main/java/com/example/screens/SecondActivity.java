65f82be323ea371c2bd954dcbeed3c1ed18e7e3b
a21oscgu <a21oscgu@student.his.se>
2022-04-27 15:24:35 +0200

    Added OnClickListener (Git account fixed... finally)

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,24 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second);
+
+        Button button = findViewById(R.id.switchscreen);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("YTA", "hej");
+            }
+        });
     }
 }
